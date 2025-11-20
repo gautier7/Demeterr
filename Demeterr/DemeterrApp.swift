23b8b2529d51f3e68e68b9b35a305d837dad30b0
gautier7 <gautier-colson@hotmail.com>
2025-11-20 23:49:57 +0800

    Initial Commit

---
+++ Demeterr/DemeterrApp.swift
@@ -0,0 +1,32 @@
+//
+//  DemeterrApp.swift
+//  Demeterr
+//
+//  Created by Gautier Colson on 20/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DemeterrApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Demeterr/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Demeterr
+//
+//  Created by Gautier Colson on 20/11/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
